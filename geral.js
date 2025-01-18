d30275be76aa255a32f06dcc4249787c3e5ce434
JoaoCarvalho-dv <joaocarvalho122020@gmail.com>
2025-01-17 22:33:12 -0300

    Primeiro commit

---
+++ geral.js
@@ -0,0 +1,12 @@
+var trocaCorLink = document.getElementsByClassName(".hoverLinks").style.background-color != "rgb(59, 59, 59)";
+
+function trocaCor(){
+    if(trocaCorLink = true){
+        document.getElementsByClassName(".hoverLinks").style.background-color == "rgb(59, 59, 59)";
+    }
+}
+
+// function tc(){
+//     document.getElementById("te").style.color = "blue";
+//     alert("test");
+// }
